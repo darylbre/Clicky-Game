b73f2f08ac6771467805a989dc38b31284be7f94
darylbre <roderickbreland@gmail.com>
2019-06-26 10:36:57 -0400

    added checkClick funtion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import drummers from "./drummers.json";
+import DrummerCard from "./Components/DrummerCard";
 
 class App extends React.Component {
-
   state = {
-    drummers: drummers
+    drummers: drummers,
+    score: 0,
+    highScore: 0,
+    checkClick: []
   };
 
+  componentDidMount = () => {
+    const drummerImg = document.getElementsByClassName("dummerImg");
+    document.addEventListener("click", drummerImg, this.handleClick);
+  };
+
+  handleClick = id => {
+    console.log(id);
+    this.setState(
+      {
+        score: this.state.score + 1
+      },
+      this.checkHighScore
+    );
+    this.checkClick(id);
+  };
+  //if id is clicked again then it set a high score and restarts the game reset checkclicked array to empty
+  checkClick = drummerClickedId => {
+    //if this.state.checkClick is empty then push this.state.drummerClickedId to the array
+    console.log(this.state.checkClick);
+    if (this.state.checkClick) {
+      this.state.checkClick.push(drummerClickedId);
+    }
+
+    this.state.checkClick.forEach(checkClickId => {
+      // checkClickId = id
+      // if checkClickId exists in the array then do nothing, else if checkClickId exists set high score, restart game and shuffle array, and set the state of checkclicked to an empty array
+      if (checkClickId === drummerClickedId) {
+      } else {
+        // then add the drummerClickedId to the checkClick array to be checked
+        this.state.checkClick.push(drummerClickedId);
+      }
+      console.log(this.state.checkClick);
+    });
+  };
+
+  checkHighScore() {
+    if (this.state.score > this.state.highScore) {
+      this.setState({
+        highScore: this.state.score
+      });
+    }
+  }
 
   render() {
     return (
-      <div>{this.state.drummers.map(drummer => (
-        <div><img src = {drummer.image}/></div>
-      ))}
+      <div className="card">
+        <h2>Score: {this.state.score}</h2>
+        <h2>High Score: {this.state.highScore}</h2>
+        <div className="img-container" />
+        {this.state.drummers.map(drummer => (
+          <DrummerCard
+            handleClick={this.handleClick}
+            id={drummer.id}
+            key={drummer.id}
+            image={drummer.image}
+          />
+        ))}
       </div>
-    // Render the function to click drummers and get score and also check if you have already clicked a drummer
     );
   }
 }
